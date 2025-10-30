b211bd2f774cdc4bf4795162de9734402d229668
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-30 19:12:58 +0800

    Create The Coin Change Problem.cs

---
+++ The Coin Change Problem.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'getWays' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. LONG_INTEGER_ARRAY c
+     */
+
+    public static long getWays(int n, List<long> c)
+    {
+        long[] ways = new long[n + 1];
+        ways[0] = 1; 
+
+        foreach (long coin in c)
+        {
+            for (int amount = (int)coin; amount <= n; amount++)
+            {
+                ways[amount] += ways[amount - (int)coin];
+            }
+        }
+
+        return ways[n];
+    }
+
+    }
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<long> c = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(cTemp => Convert.ToInt64(cTemp)).ToList();
+
+        // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+        long ways = Result.getWays(n, c);
+
+        textWriter.WriteLine(ways);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
